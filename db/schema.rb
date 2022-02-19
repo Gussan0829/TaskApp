566dbf1ffe943b28731957402ed41b815944166b
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-19 23:32:33 +0900

    devise マイグレーションファイルの実行

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,6 @@ ActiveRecord::Schema.define(version: 2022_02_19_120503) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "first_name_kan", null: false
-    t.string "family_name_kan", null: false
-    t.string "first_name_kana", null: false
-    t.string "family_name_kana", null: false
-    t.date "birth", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
