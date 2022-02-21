9ab3dc3f996420f2575b587f46b6534527755e98
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-21 18:41:20 +0900

    bootstrapの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,4 @@
-import 'bootstrap';
-import '../stylesheets/application';
+
 // 中略
 
 // This file is automatically compiled by Webpack, along with any other files
@@ -11,6 +10,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import 'bootstrap';
+import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
