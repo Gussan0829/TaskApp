c1dbb3f38f1abd842eb9b5a09778cfde4de25def
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-23 00:17:33 +0900

    トップページの修正
    コントローラーへのCRUDの追加
    トップページのviewの追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,7 @@
 class ListsController < ApplicationController
+before_action :find_list, only: [:edit, :update, :destroy]
+protect_from_forgery :except => [:destroy]
+
   def new
     @list = List.new
   end
@@ -12,8 +15,28 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+   @list.destroy
+   redirect_to root_path
+  end
+
   private
   def list_params
     params.require(:list).permit(:title).merge(user: current_user)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
   end
 end
