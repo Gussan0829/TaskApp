f67021df6ecad99934ce410e87bfbb0468ac29e6
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-23 00:24:38 +0900

    Task_cardsコントローラーの作成

--- app/controllers/task_cards_controller.rb
+++ app/controllers/task_cards_controller.rb
@@ -1,2 +1,20 @@
 class TaskCardsController < ApplicationController
+  def new
+    @task_card = TaskCard.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @task_card = TaskCard.new(task_card_params)
+    if @task_card.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_card_params
+    params.require(:task_card).permit(:title, :memo, :list_id)
+  end
 end
