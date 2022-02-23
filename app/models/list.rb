27f67700f6636ef02e1f6e94b527d5db46f2bcab
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-23 21:18:30 +0900

    task_cardsテーブル新規投稿機能の作成

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   validates :title, presence: true
 
   belongs_to :user
-  has_many :cards, dependent: :destroy
+  has_many :task_cards, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tasks#index'
   resources :lists, only: [:new, :create, :edit, :update, :destroy] do
-    resources :task_card, only: [:new, :create, :edit, :update, :destroy]
+    resources :task_cards, only: [:new, :create, :edit, :update, :destroy]
   end
 end
