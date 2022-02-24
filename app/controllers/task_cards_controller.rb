3e2c787203586c6a1e7b2b0a52194cf0d10a7152
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-24 20:22:18 +0900

    task_card 編集機能の実装

--- app/controllers/task_cards_controller.rb
+++ app/controllers/task_cards_controller.rb
@@ -1,4 +1,6 @@
 class TaskCardsController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+  
   def new
     @task_card = TaskCard.new
     @list = List.find(params[:list_id])
@@ -14,11 +16,25 @@ class TaskCardsController < ApplicationController
   end
 
   def show
-    @task_card = TaskCard.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @task_card.update(task_card_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
   def task_card_params
     params.require(:task_card).permit(:title, :memo, :list_id)
+  end
+
+  def set_task
+    @task_card = TaskCard.find(params[:id])
   end
 end
